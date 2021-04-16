36a7eedf68f488749b47a6c3f417be4b69d9ab0f
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 18:39:46 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -58,6 +58,6 @@ pause.addEventListener('click',function(e){
 
 //add comments
 submit.addEventListener('click',function(e){
-  comment.innerText = commentInput.value;
+  comments.innerText = commentInput.value;
   event.disableDefault();
 })
