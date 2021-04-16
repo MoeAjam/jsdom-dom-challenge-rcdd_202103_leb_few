1762a8d7d36e7168412c06cc5a56967827bd06ff
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 18:39:14 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -6,7 +6,7 @@ let likes = document.getElementsByClassName('likes')[0];
 let pause = document.getElementById('pause');
 let submit = document.getElementById('submit');
 let commentInput = document.getElementById('comment-input');
-let comments = document.getElementById('comments');
+let comments = document.getElementsByClassName('comments')[0];
 //incriment by 1 every second
 let a = setTimeout(start, 5000);
 let i = 1;
