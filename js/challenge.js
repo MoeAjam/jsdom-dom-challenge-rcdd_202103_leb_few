eb99f99fdfb6dcd6947a6ddd9e8564ae49f0ce60
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 17:59:53 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,9 +2,9 @@ let counter = document.getElementById('counter');
 
 let i = 1;
 let interval = setInterval( increment, 1000);
-while (true){
+
 function increment(){
     i = i % 360 + 1;
 }
-}
+
 counter.innerText = i;
